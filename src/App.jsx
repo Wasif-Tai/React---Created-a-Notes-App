88da97ad9bb95279140f2520a26fd916e4238d65
Wasif Tai <muhammadwasiftai@gmail.com>
2026-09-22 10:34:43 +0500

    {UPDATE} Added Multiple Functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import { MdOutlineDelete } from "react-icons/md";
+import { FaRegCheckCircle } from "react-icons/fa";
 
 function Todos() {
   // Koi bhi aisi value jiske change hone se UI update hogi usko ham state main rakhenge.
@@ -21,6 +23,19 @@ function Todos() {
     setTodos([obj, ...todos]);
     setTodoInp("");
   };
+
+  // why we use ... because we want to create a copy of the todos array and not modify the original array directly.
+  // ye is liye kerte hai take react ko pata chale ki state change hui hai aur UI rerender ho.
+  const markDone = (ind) => {
+    let arr = [...todos];
+    arr[ind].isDone = !arr[ind].isDone;
+    setTodos(arr);
+  };
+  const deleteTodo = (ind) => {
+    let arr = [...todos];
+    arr.splice(ind, 1);
+    setTodos(arr);
+  };
   return (
     <main className="todo-page">
       <section className="todo-panel">
@@ -36,10 +51,40 @@ function Todos() {
           <button onClick={addTodo}>Add</button>
         </div>
         <div className="todo-list">
-          {todos.map((data, index) => {
+          {todos.map((data, ind) => {
+            console.log("ind", ind, "data", data);
             return (
-              <div className="todo-item" key={data.title}>
-                {index + 1 + ")"} {data.title}
+              <div
+                className="todo-item"
+                key={data.title}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  textDecoration: data.isDone ? "line-through" : "none",
+                }}
+              >
+                <span>
+                  {ind + 1 + ")"} {data.title}
+                </span>
+                <div className="todo-actions">
+                  <button
+                    onClick={() => markDone(ind)}
+                    title={data.isDone ? "Mark as not done" : "Mark as done"}
+                    aria-label={data.isDone ? "Mark as not done" : "Mark as done"}
+                    className="todo-action"
+                  >
+                    <FaRegCheckCircle />
+                  </button>
+                  <button
+                    onClick={() => deleteTodo(ind)}
+                    title="Delete todo"
+                    aria-label="Delete todo"
+                    className="todo-action todo-action-delete"
+                  >
+                    <MdOutlineDelete />
+                  </button>
+                </div>
               </div>
             );
           })}
